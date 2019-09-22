5163bdb4f03322a8bd1d62e1f98a68f0c03c0093
theomega <theo@wieenrt-hamburg.de>
2019-09-22 21:09:41 +0200

    8

--- api.php
+++ api.php
@@ -7,7 +7,6 @@
     //this is a sample query which gets some data, the order by part shuffles the results
     $query = "SELECT rack FROM racks where length>5 order by random() limit 1";
     $row = sqlite_fetch_array($query, SQLITE_ASSOC);
-    echo $row;
     
     //this next line could actually be used to provide user_given input to the query to 
     //avoid SQL injection attacks
