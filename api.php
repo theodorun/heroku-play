d20b4d015411774c1ea23d255768b06d0227673d
theomega <theo@wieenrt-hamburg.de>
2019-09-22 21:51:48 +0200

    sad

--- api.php
+++ api.php
@@ -22,7 +22,7 @@
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    print_r($results[0]);
+    print_r($results[0].['rack']);
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
     //making to this line means everything was great with this request
     header('HTTP/1.1 200 OK');
