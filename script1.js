670b44faf847bd2e9b21f91c16488821c7232eea
theomega <theo@wieenrt-hamburg.de>
2019-09-23 06:01:26 +0200

    wokring local

--- script1.js
+++ script1.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 
         });
         //let onlyrack=racks[0][rack];
-        console.log(racks[0]);
+        console.log(racks[0][rack]);
         //$("#bingos").append(`<h2>${onlyrack}</h2>`);
         arrwords.sort();
         console.log(arrwords);
