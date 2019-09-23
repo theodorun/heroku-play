c975356824850113c7998de4e371f1756d0e4b50
theomega <theo@wieenrt-hamburg.de>
2019-09-23 05:57:30 +0200

    wokring local

--- script1.js
+++ script1.js
@@ -16,7 +16,8 @@ $(document).ready(function () {
             });
 
         });
-        $("#bingos").append(`<h2>${racks[0][rack]}</h2>`);
+        let onlyrack=racks[0][rack];
+        $("#bingos").append(`<h2>${onlyrack}</h2>`);
         arrwords.sort();
         console.log(arrwords);
         for (const c of arrwords) {
